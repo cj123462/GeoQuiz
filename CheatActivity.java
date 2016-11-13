1be85c40aaca8cef97a956fcf10a1a1ac7d60f5d
cj123462 <cj123462@gmail.com>
2016-11-13 12:48:15 +0200

    Add files via upload

--- CheatActivity.java
+++ CheatActivity.java
@@ -1,13 +1,80 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CheatActivity extends AppCompatActivity {
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+    private static final String TAG = "CheatActivity";
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private Toast mJudgeToast;
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent i = new Intent(packageContext, CheatActivity.class);
+        i.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return i;
+    }
+
+    private void makeJudgeToast(){
+        int msg=R.string.judgment_toast;
+        if(mJudgeToast!=null) mJudgeToast.cancel();
+        mJudgeToast=Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        mJudgeToast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        mShowAnswer = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mAnswerIsTrue){
+                    mAnswerTextView.setText(R.string.true_button);
+                } else
+                {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                makeJudgeToast();
+
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
