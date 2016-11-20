9649e6a7b8b9466e0f2203ef156e3957d7c31142
cj123462 <cj123462@gmail.com>
2016-11-20 14:54:49 +0200

    Add files via upload

--- CheatActivity.java
+++ CheatActivity.java
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,10 +17,14 @@ import android.widget.Toast;
 public class CheatActivity extends AppCompatActivity {
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
+    private TextView mApiTextView;
     private Button mShowAnswer;
     private static final String TAG = "CheatActivity";
     private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
-    private Toast mJudgeToast;
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
+    private boolean mCheated;
+    private int mAnswer;
+    private static final String IS_CHEAT = "cheat";
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent i = new Intent(packageContext, CheatActivity.class);
@@ -24,12 +32,11 @@ public class CheatActivity extends AppCompatActivity {
         return i;
     }
 
-    private void makeJudgeToast(){
-        int msg=R.string.judgment_toast;
-        if(mJudgeToast!=null) mJudgeToast.cancel();
-        mJudgeToast=Toast.makeText(this, msg, Toast.LENGTH_SHORT);
-        mJudgeToast.show();
+    public static boolean wasAnswerShown(Intent result){
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
     }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +44,62 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        mApiTextView = (TextView) findViewById(R.id.api_version);
         mShowAnswer = (Button) findViewById(R.id.show_answer_button);
         mShowAnswer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(mAnswerIsTrue){
-                    mAnswerTextView.setText(R.string.true_button);
+                    mAnswer=R.string.true_button;
+                    mAnswerTextView.setText(mAnswer);
                 } else
                 {
-                    mAnswerTextView.setText(R.string.false_button);
+                    mAnswer=R.string.false_button;
+                    mAnswerTextView.setText(mAnswer);
                 }
-                makeJudgeToast();
+                mCheated=true;
+                setAnswerShownResult(true);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                int cx = mShowAnswer.getWidth() / 2;
+                int cy = mShowAnswer.getHeight() / 2;
+                float radius = mShowAnswer.getWidth();
+                Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mAnswerTextView.setVisibility(View.VISIBLE);
+                        mShowAnswer.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();}else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
 
             }
         });
+        if(savedInstanceState!=null){
+            mCheated=savedInstanceState.getBoolean(IS_CHEAT);
+        }
+        String mVersion = Build.VERSION.SDK;
+        mApiTextView.setText("API level"+mVersion);
     }
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putBoolean(IS_CHEAT, mCheated);
+
+    }
+
     @Override
     public void onStart() {
         super.onStart();
